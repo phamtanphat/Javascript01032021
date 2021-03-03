8691ed121ed5dacc51977dad933d87cd9764ee3c
phamtanphat <phatdroid94@gmail.com>
2021-03-03 20:39:57 +0700

    sự khác biệt của let và var

--- buoi1.js
+++ buoi1.js
@@ -4,4 +4,23 @@
 //     + File name
 
 // Hello
-ưqewqe
+
+// 1 : Khai báo
+
+// Sự khong chặt chẽ
+// re - create : Bị khởi tạo lại
+// let a = 10
+// let a = 11
+
+// scope : pham vi ({})
+
+// if (true){
+//     var a = 10
+// }
+
+// hoisting 
+
+console.log(a)
+let a = 10
+
+
