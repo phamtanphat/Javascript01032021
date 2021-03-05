e7721af669933eb057ddf347acb725fc0cc3db8e
phamtanphat <phatdroid94@gmail.com>
2021-03-05 20:22:19 +0700

    array method

---
+++ buoi2.js
@@ -0,0 +1,18 @@
+// 13 : Array method
+// const arrNums = [1,2,3,4,5]
+
+// Thêm dữ liệu ở cuối mảng
+// arrNums.push(6)
+// Xoá ở cuối
+// arrNums.pop()
+// Thêm ở đầu
+// arrNums.unshift(0)
+// Xoá ở đầu
+// arrNums.shift()
+// Xoá ở giữa
+// arrNums.splice(1,1)
+// Thêm ở giữa
+// arrNums.splice(1,0,6,7,8,9,10)
+// arrNums.splice(-2,1)
+
+// console.log(arrNums)
