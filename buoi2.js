cf756fa900ab3dd1c25462fb190b861a458018ae
phamtanphat <phatdroid94@gmail.com>
2021-03-05 20:58:51 +0700

    bài tập hàm map

--- buoi2.js
+++ buoi2.js
@@ -34,14 +34,41 @@
 //15 : Hàm map (transform mảng)
 // 1 - Thay đổi giá trị của phần tử, không thay đổi kích thươc mảng
 // 2 - Không gắn sự thay đổi vào mảng cũ, gắn sự thay đổi vào mảng mới
-const arrNums = [1,2,3,4,5,6,7,8,9,10]
+// const arrNums = [1,2,3,4,5,6,7,8,9,10]
 
-const newArrNums = arrNums.map(function(number , index) {
-    return number*2
-})
+// const newArrNums = arrNums.map(function(number , index) {
+//     return number*2
+// })
 
-console.log(arrNums)
-console.log(newArrNums)
+// console.log(arrNums)
+// console.log(newArrNums)
+
+function namesOnly(arr) {
+    const newArr = arr.map(function(person) {
+        return person.name
+    })
+    console.log(newArr)
+}
+
+namesOnly([
+    {
+        name:"Teo",
+        age : 20
+    },
+    {
+        name:"Ti",
+        age : 5
+    },
+    {
+        name:"Tun",
+        age : 8
+    },
+    {
+        name:"Tuan",
+        age : 10
+    },
+])
+
 
 
 
